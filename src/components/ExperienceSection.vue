20e7307e971434bd379777befd644defc9c7e4fb
Max Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2024-05-21 23:19:19 +0300

    exp icon reset

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -16,87 +16,24 @@
                     </div>
 
                     <div class="">
-                        <div class="flex items-center gap-3 mb-4">
+                        <div class="flex items-center gap-3 mb-4 ml-2">
                             <div class="flex items-center justify-center">
                                 <!-- <span class="text-[96px] leading-3 text-[#F0BF6C] mb-3">•</span> -->
-                                <svg width="32px" height="32px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M0.877075 7.49988C0.877075 3.84219 3.84222 0.877045 7.49991 0.877045C11.1576 0.877045 14.1227 3.84219 14.1227 7.49988C14.1227 11.1575 11.1576 14.1227 7.49991 14.1227C3.84222 14.1227 0.877075 11.1575 0.877075 7.49988ZM7.49991 1.82704C4.36689 1.82704 1.82708 4.36686 1.82708 7.49988C1.82708 10.6329 4.36689 13.1727 7.49991 13.1727C10.6329 13.1727 13.1727 10.6329 13.1727 7.49988C13.1727 4.36686 10.6329 1.82704 7.49991 1.82704Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".05"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M6.78296 13.376C8.73904 9.95284 8.73904 5.04719 6.78296 1.62405L7.21708 1.37598C9.261 4.95283 9.261 10.0472 7.21708 13.624L6.78296 13.376Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".1"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M7.28204 13.4775C9.23929 9.99523 9.23929 5.00475 7.28204 1.52248L7.71791 1.2775C9.76067 4.9119 9.76067 10.0881 7.71791 13.7225L7.28204 13.4775Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".15"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M7.82098 13.5064C9.72502 9.99523 9.72636 5.01411 7.82492 1.50084L8.26465 1.26285C10.2465 4.92466 10.2451 10.085 8.26052 13.7448L7.82098 13.5064Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".2"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M8.41284 13.429C10.1952 9.92842 10.1957 5.07537 8.41435 1.57402L8.85999 1.34729C10.7139 4.99113 10.7133 10.0128 8.85841 13.6559L8.41284 13.429Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".25"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M9.02441 13.2956C10.6567 9.8379 10.6586 5.17715 9.03005 1.71656L9.48245 1.50366C11.1745 5.09919 11.1726 9.91629 9.47657 13.5091L9.02441 13.2956Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".3"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M9.66809 13.0655C11.1097 9.69572 11.1107 5.3121 9.67088 1.94095L10.1307 1.74457C11.6241 5.24121 11.6231 9.76683 10.1278 13.2622L9.66809 13.0655Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".35"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M10.331 12.7456C11.5551 9.52073 11.5564 5.49103 10.3347 2.26444L10.8024 2.0874C12.0672 5.42815 12.0659 9.58394 10.7985 12.9231L10.331 12.7456Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".4"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M11.0155 12.2986C11.9938 9.29744 11.9948 5.71296 11.0184 2.71067L11.4939 2.55603C12.503 5.6589 12.502 9.35178 11.4909 12.4535L11.0155 12.2986Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".45"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M11.7214 11.668C12.4254 9.01303 12.4262 5.99691 11.7237 3.34116L12.2071 3.21329C12.9318 5.95292 12.931 9.05728 12.2047 11.7961L11.7214 11.668Z"
-    fill="#F0BF6C"
-  />
-  <path
-    opacity=".5"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
-    d="M12.4432 10.752C12.8524 8.63762 12.8523 6.36089 12.4429 4.2466L12.9338 4.15155C13.3553 6.32861 13.3554 8.66985 12.9341 10.847L12.4432 10.752Z"
-    fill="#F0BF6C"
-  />
-</svg>
+                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
+                                    viewBox="0 0 1280.000000 1280.000000" preserveAspectRatio="xMidYMid meet">
+                                    <metadata>
+                                        Created by potrace 1.15, written by Peter Selinger 2001-2017
+                                    </metadata>
+                                    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
+                                        fill="#F0BF6C" stroke="none">
+                                        <path d="M6125 12794 c-27 -2 -120 -8 -205 -14 -957 -68 -1973 -394 -2829
+-908 -1759 -1055 -2891 -2894 -3073 -4992 -16 -187 -16 -773 0 -960 89 -1022
+397 -1978 918 -2841 1056 -1754 2890 -2879 4984 -3061 187 -16 773 -16 960 0
+1016 88 1973 396 2829 910 1759 1055 2891 2894 3073 4992 16 187 16 773 0 960
+-118 1362 -635 2623 -1495 3648 -1071 1277 -2647 2092 -4347 2248 -154 14
+-704 26 -815 18z" />
+                                    </g>
+                                </svg>
                             </div>
                             <div>
                                 <h3 class="text-[32px] ">GoToInc</h3>
@@ -140,17 +77,22 @@
                     </div>
 
                     <div class="">
-                        <div class="flex items-center gap-3 mb-4">
+                        <div class="flex items-center gap-3 mb-4 ml-2">
                             <div class="flex items-center justify-center ">
-                                <!-- <span class="text-[96px] leading-3 text-[#F0BF6C] mb-3 blur-[2px]">•</span> -->
-                                <svg fill="#F0BF6C" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-                                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28px"
-                                    height="32px" viewBox="0 0 72.668 72.668"
-                                    style="enable-background:new 0 0 72.668 72.668;" xml:space="preserve">
-                                    <g>
-                                        <path d="M0,36.334c0,20.034,16.299,36.334,36.334,36.334s36.334-16.3,36.334-36.334S56.369,0,36.334,0S0,16.3,0,36.334z
-		 M23.163,6.195c12.94-8.23,31.183-2.797,40.666,12.111c9.481,14.907,6.671,33.733-6.269,41.963
-		c-12.939,8.231-31.184,2.799-40.667-12.108C7.412,33.253,10.224,14.426,23.163,6.195z" />
+
+                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
+                                    viewBox="0 0 1280.000000 1280.000000" preserveAspectRatio="xMidYMid meet">
+                                    <metadata>
+                                        Created by potrace 1.15, written by Peter Selinger 2001-2017
+                                    </metadata>
+                                    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
+                                        fill="#F0BF6C" stroke="none">
+                                        <path d="M6125 12794 c-27 -2 -120 -8 -205 -14 -957 -68 -1973 -394 -2829
+-908 -1759 -1055 -2891 -2894 -3073 -4992 -16 -187 -16 -773 0 -960 89 -1022
+397 -1978 918 -2841 1056 -1754 2890 -2879 4984 -3061 187 -16 773 -16 960 0
+1016 88 1973 396 2829 910 1759 1055 2891 2894 3073 4992 16 187 16 773 0 960
+-118 1362 -635 2623 -1495 3648 -1071 1277 -2647 2092 -4347 2248 -154 14
+-704 26 -815 18z" />
                                     </g>
                                 </svg>
                             </div>
@@ -192,9 +134,24 @@
                     </div>
 
                     <div class="">
-                        <div class="flex items-center gap-3 mb-4">
+                        <div class="flex items-center gap-3 mb-4 ml-2">
                             <div class="flex items-center justify-center">
-                                <span class="text-[96px] leading-3 text-[#F0BF6C] mb-3">•</span>
+                                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
+                                    viewBox="0 0 1280.000000 1280.000000" preserveAspectRatio="xMidYMid meet">
+                                    <metadata>
+                                        Created by potrace 1.15, written by Peter Selinger 2001-2017
+                                    </metadata>
+                                    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
+                                        fill="#F0BF6C" stroke="none">
+                                        <path d="M6125 12794 c-27 -2 -120 -8 -205 -14 -957 -68 -1973 -394 -2829
+-908 -1759 -1055 -2891 -2894 -3073 -4992 -16 -187 -16 -773 0 -960 89 -1022
+397 -1978 918 -2841 1056 -1754 2890 -2879 4984 -3061 187 -16 773 -16 960 0
+1016 88 1973 396 2829 910 1759 1055 2891 2894 3073 4992 16 187 16 773 0 960
+-118 1362 -635 2623 -1495 3648 -1071 1277 -2647 2092 -4347 2248 -154 14
+-704 26 -815 18z" />
+                                    </g>
+                                </svg>
+
                             </div>
                             <div>
                                 <h3 class="text-[32px] ">GoIT</h3>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,23 @@
   <main class="relative ">
 
 
-    
-      <div class="flex justify-center items-center bg-common fixed left-0 right-0 z-[10]">
+
+    <div class="flex justify-center items-center bg-common fixed left-0 right-0 z-[10] shadow-lg">
       <div class="w-full px-5 md:px-6 py-6 lg:w-[1000px] xl:w-[1240px] 2xl:w-[1520px]">
         <div class="flex gap-4 text-xl justify-center sm:justify-start">
           <!-- <button type="button" class="text-white" @click="scrollTo(hero)">Home</button> -->
-          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300" @click="scrollTo(experience)">Experience</button>
-          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300" @click="scrollTo(portfolio)">Portfolio</button>
-          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300" @click="scrollTo(skills)">Skills</button>
-          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300" @click="scrollTo(contacts)">Contacts</button>
+          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300"
+            @click="scrollTo(experience)">Experience</button>
+          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300"
+            @click="scrollTo(portfolio)">Portfolio</button>
+          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300"
+            @click="scrollTo(skills)">Skills</button>
+          <button type="button" class="text-white hover:text-[#F0BF6C] duration-300"
+            @click="scrollTo(contacts)">Contacts</button>
         </div>
       </div>
     </div>
-  
+
 
 
     <section ref="hero">
