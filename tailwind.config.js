77c4d60c736573eab0ec2c9c419ee6a613213384
Max Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2024-05-27 03:51:14 +0300

    bg optimized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
-        'common': "url(../../public/try2.png)"
+        common: 'url(../../public/try2.jpg)'
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
