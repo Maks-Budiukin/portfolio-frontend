49b6c23a797bb0208ded1a2fc942aafab407804a
Max Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2024-05-28 23:16:12 +0300

    router fix

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -94,12 +94,10 @@
             </div>
 
         </div>
-        <RouterView />
     </div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
 </script>
 
 <style lang="scss" scoped>
